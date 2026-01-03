a2f7c518ac82b7ee5bb858934ea3d7e2fa2cf556
nischay-chauhan <masternischay@outlook.com>
2026-01-03 22:17:09 +0530

    error fixed

--- 1000/k.cpp
+++ 1000/k.cpp
@@ -1,22 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int t;
-    cin >> t;
+int main()
+{
+	int t;
+	cin >> t; 
+	while (t--)
+	{
+		string s;
+		cin >> s;
+		int n = s.size();
+		int c0 = 0, c1 = 0;
 
-    while(t--){
-        string s;
-        cin >> s;
+		for (int i = 0; i < n; i++)
+		{
+			if (s[i] == '0')
+				c0++;
+			else
+				c1++;
+		}
 
-        int c0 = 0 , c1 = 0;
-        for(char c : s){
-            if(c == '0') c0++;
-            else c1++;
-        }
+		int length_of_t = 0;
+		for (int i = 0; i < n; i++)
+		{
+			if (s[i] == '0' && c1 > 0)
+			{
+				c1--; 
+				length_of_t++; 
+			}
+			else if (s[i] == '1' && c0 > 0)
+			{
+				c0--; 
+				length_of_t++; 
+			}
+			else
+			{
+				break;
+			}
+		}
 
-        cout << max(c0, c1) << "\n";
-    }
-
-    return 0;
+		cout << n - length_of_t << "\n";
+	}
+	return 0;
 }
